446005869d08397a35a388bf9ed5fd9e4ab56bba
Jon Cuyler <jcuyler@appliedmedical.com>
2025-03-31 14:21:38 +0200

    add initial project setup with components, configuration, and utilities

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { passkey } from "better-auth/plugins/passkey";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    }),
+    emailAndPassword: {
+        enabled: true,
+        async sendResetPassword(data, request) {
+
+        },
+    },
+    plugins: [
+        passkey(),
+    ],
+});
